e9a9ca29a4d46289575596ede54a404bc263fcb0
marianoariassoy <hola@marianoarias.soy>
2023-02-02 16:00:33 -0300

    primera entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Comisión 39500</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    <div class="container mx-auto px-4 py-4">
+      <NavBar />
+      <ItemListContainer greeting="Welcome to MyReactEcommerce 🚀" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ItemListContainer = ({ greeting }) => {
+  return <main className="text-center px-4 py-4">{greeting}</main>;
+};
+
+export default ItemListContainer;
