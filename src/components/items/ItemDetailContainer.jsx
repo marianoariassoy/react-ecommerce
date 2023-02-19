46b1d4c330c3310aef1ab631f558c61d6948637f
Mariano Arias <hola@marianoarias.soygit config --global user.name Mariano>
2023-02-19 15:26:27 -0300

    2da entrega

--- src/components/items/ItemDetailContainer.jsx
+++ src/components/items/ItemDetailContainer.jsx
@@ -11,7 +11,7 @@ const ItemDetailContainer = () => {
   useEffect(() => {
     getItems(url).then((data) => {
       if (id) {
-        const data = data.filter((item) => item.category === parseInt(id));
+        const result = data.filter((item) => item.category === parseInt(id));
         setData(result);
       } else {
         setData(data);
